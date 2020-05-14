0c190315d6d085c8ef8aeeb1bd6ace6ad83e93e6
haden <haden.lee@molocoads.com>
2020-05-14 12:14:02 -0700

    ok

--- java/student/src/main/java/edu/usfca/cs673/Task2.java
+++ java/student/src/main/java/edu/usfca/cs673/Task2.java
@@ -4,12 +4,6 @@ public class Task2 {
   public static int getNumberOfInversions(int[] A) {
     // TODO: Implement this.
     int n = A.length, cnt = 0;
-    for (int i = 0; i < n; i++) {
-      for (int j = i + 1; j < n; j++) {
-        if (A[i] > A[j])
-          cnt++;
-      }
-    }
     return cnt;
   }
 }
